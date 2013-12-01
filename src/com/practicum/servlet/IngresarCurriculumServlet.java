756b9b8e64265f7e2f7949d75f0aa9ee1775da5f
Benjamin Jimenez <benja.jimenez.a@gmail.com>
2013-12-01 18:54:59 -0300

    v1.18
    Base para el Servlet de ingreso de curriculum, no fucional

---
+++ src/com/practicum/servlet/IngresarCurriculumServlet.java
@@ -0,0 +1,75 @@
+package com.practicum.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.practicum.dao.CurriculumDAO;
+import com.practicum.util.*;
+
+public class IngresarCurriculumServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	           throws ServletException, java.io.IOException {
+
+		try
+		{	
+			Boolean[] flag = new Boolean[10];//tamaño depende de la cantidad de datos del formulario
+			
+			HttpSession session = request.getSession(true);
+			AlumnoBean alumno = ((AlumnoBean) (session.getAttribute("currentSessionUser")));
+			
+			System.out.println("RUT SESION: "+alumno.getRut());
+			System.out.println("TIPO SESION: "+alumno.getType());
+			
+			//ACA CAPTURAR LOS DATOS DEL FORMULARIO
+			
+//			String primerNombre =request.getParameter("establecimiento");
+//			String segundoNombre =request.getParameter("segundoNombre");
+//			String paterno =request.getParameter("paterno");
+//			String materno =request.getParameter("materno");
+//			String civil =request.getParameter("civil");
+//			String direccion =request.getParameter("direccion");
+//			String nacionalidad =request.getParameter("nacionalidad");
+//			String nacimiento =request.getParameter("nacimiento"); //FALTA GUARDAR ESTE DATO !
+//			String referencia =request.getParameter("referencia");
+//			String datosExtra =request.getParameter("datosExtra");
+			
+			//PASO DE DATOS
+			
+			Curriculum curriculum = new Curriculum();
+			List<AreaInteres> areas = new ArrayList<AreaInteres>();
+			List<Idioma> idiomas = new ArrayList<Idioma>();
+			List<DatoAcademico> datosAcademicos= new ArrayList<DatoAcademico>();
+			List<HistorialLaboral> laborales = new ArrayList<HistorialLaboral>();
+			
+			// SE ASOCIAN LOS DATOS AL CURRICULUM
+			curriculum.setId(1);
+			curriculum.setRun(alumno.getRut());
+			curriculum.setActive(true);
+			curriculum.setDatosAcademicos(datosAcademicos);
+			curriculum.setIdiomas(idiomas);
+			curriculum.setIntereses(areas);
+			curriculum.setLaborales(laborales);
+			curriculum.setConfidencial(true); //ajustar
+			curriculum.setUltimaActualizacion(null); //dar fecha
+			
+			// Se inserta el curriculum al DAO
+			alumno.setCurriculum(curriculum);
+			CurriculumDAO.insertCurriculum(alumno);
+			
+			
+			
+			
+		}catch (Throwable theException) 	    
+		{
+		     System.out.println(theException); 
+		}
+	}
+}
+		       
