2f89b0b737ccb2d3155555c38bb1f4ae9c0bec9b
LethalDrumsXxl <s.segovia.c@gmail.com>
2013-12-02 17:14:06 -0400

    v.1.21
    Base de datos con inserciones para las pruebas

--- Practicum.sql
+++ Practicum.sql
@@ -33,7 +33,7 @@ create table alumnos(
 	primary key(run),
 	foreign key(run) references usuarios(id) on delete cascade on update cascade
 	);	
-	
+		
 -- Tabla que contiene los curriculums	
 create table curriculums(
 	id int,
@@ -44,7 +44,7 @@ create table curriculums(
 	foreign key(run) references alumnos(run) on delete cascade on update cascade,
 	primary key(run,id)
 	);
-
+	
 -- Areas Principales de interes y a que pertenecen
 create table areas_interes(
 	id int auto_increment,
@@ -54,31 +54,7 @@ create table areas_interes(
 	redes int,
 	primary key(id)
 	);
-	
-insert into areas_interes values(null,'Lenguaje Java',1,0,0);
-insert into areas_interes values(null,'Lenguaje Ruby',1,0,0);
-insert into areas_interes values(null,'Lenguaje Python',1,0,0);
-insert into areas_interes values(null,'Lenguaje C',1,0,0);
-insert into areas_interes values(null,'Lenguaje C++',1,0,0);
-insert into areas_interes values(null,'Lenguaje C#',1,0,0);
-insert into areas_interes values(null,'UML',1,0,0);
-insert into areas_interes values(null,'IDE Eclipse',1,0,0);
-insert into areas_interes values(null,'IDE NetBeans',1,0,0);
-insert into areas_interes values(null,'IDE TopBraid',1,0,0);
-insert into areas_interes values(null,'Lenguaje HTML',1,0,0);
-insert into areas_interes values(null,'Lenguaje CSS',1,0,0);
-insert into areas_interes values(null,'Lenguaje XML',1,0,0);
-insert into areas_interes values(null,'Lenguaje RDF',1,0,0);
-insert into areas_interes values(null,'Lenguaje WSDL',1,0,0);
-insert into areas_interes values(null,'IDE Knime',0,1,0);
-insert into areas_interes values(null,'MySql',0,1,0);
-insert into areas_interes values(null,'PostgresSql',0,1,0);
-insert into areas_interes values(null,'Lenguaje Prolog',0,1,0);
-insert into areas_interes values(null,'Workbench',0,1,0);
-insert into areas_interes values(null,'Lenguaje PHP',1,1,0);
-insert into areas_interes values(null,'IDE Core',0,0,1);
 
-	
 -- Compuesta de areas de interes y curriculum	
 create table curriculums_areas_interes(
 	id int,
@@ -120,8 +96,6 @@ create table idiomas(
 	primary key(id)
 	);
 	
-insert into idiomas values (null,'Ingles') , (null,'Portugues') , (null,'Frances') , (null,'Aleman') , (null,'Chino Mandarin');
-	
 create table manejo_idiomas(
 	id int,
 	run int,
@@ -131,7 +105,7 @@ create table manejo_idiomas(
 	foreign key(run,id_curriculum) references curriculums(run,id) on delete cascade on update cascade,
 	primary key(id,run,id_curriculum)
 	);
-	
+
 create table empresas(
 	rut int,
 	email varchar(50),
@@ -145,7 +119,7 @@ create table empresas(
 	foreign key(rut) references usuarios(id) on delete cascade on update cascade,
 	primary key(rut)
 	);
-	
+		
 create table profesores(
 	run int,
 	email varchar(50),
@@ -157,7 +131,7 @@ create table profesores(
 	foreign key(run) references usuarios(id) on delete cascade on update cascade,
 	primary key(run)
 	);
-	
+
 create table recomendaciones(
 	run_profesor int,
 	run_alumno int,
@@ -168,16 +142,18 @@ create table recomendaciones(
 );
 
 create table asignaturas(
-	id int auto_increment,
+	id int,
 	asignatura varchar(50),
-	descripcion text,
+	desarrollo int,
+	base_de_datos int,
+	redes int,
 	primary key(id)
 );
 
 create table notas_alumnos(
 	run int,
 	id_asignatura int,
-	nota int,
+	nota float,
 	foreign key(run) references alumnos(run) on delete cascade on update cascade,
 	foreign key(id_asignatura) references asignaturas(id) on delete cascade on update cascade,
 	primary key(run,id_asignatura)
@@ -226,3 +202,148 @@ create trigger borra_profesor before delete on profesores for each row
 ??
 
 delimiter ;
+
+-- Inserciones para el test del programa
+
+insert into alumnos values(123456789,'gatin@test.com',AES_ENCRYPT('gatin','gatin'),'Gatin','Gatuso','Cat','Kitty','Soltero','Villa Gatito #001','Gatunesca','2000-03-22','Soy un gatito bien sensual','Hola, soy el dato adicional');
+
+insert into curriculums values (1,123456789,'true','2013-03-01','true');
+
+insert into areas_interes values(null,'Lenguaje Java',1,0,0);
+insert into areas_interes values(null,'Lenguaje Ruby',1,0,0);
+insert into areas_interes values(null,'Lenguaje Python',1,0,0);
+insert into areas_interes values(null,'Lenguaje C',1,0,0);
+insert into areas_interes values(null,'Lenguaje C++',1,0,0);
+insert into areas_interes values(null,'UML',1,0,0);
+insert into areas_interes values(null,'IDE Eclipse',1,0,0);
+insert into areas_interes values(null,'IDE NetBeans',1,0,0);
+insert into areas_interes values(null,'IDE TopBraid',1,0,0);
+insert into areas_interes values(null,'Lenguaje HTML',1,0,0);
+insert into areas_interes values(null,'Lenguaje CSS',1,0,0);
+insert into areas_interes values(null,'Lenguaje XML',1,0,0);
+insert into areas_interes values(null,'Lenguaje RDF',1,0,0);
+insert into areas_interes values(null,'Lenguaje WSDL',1,0,0);
+insert into areas_interes values(null,'IDE Knime',0,1,0);
+insert into areas_interes values(null,'MySql',0,1,0);
+insert into areas_interes values(null,'PostgresSql',0,1,0);
+insert into areas_interes values(null,'Lenguaje Prolog',0,1,0);
+insert into areas_interes values(null,'Workbench',0,1,0);
+insert into areas_interes values(null,'Lenguaje PHP',1,1,0);
+insert into areas_interes values(null,'IDE Core',0,0,1);
+
+	
+insert into curriculums_areas_interes values(1,123456789,1);
+insert into curriculums_areas_interes values(2,123456789,1);
+insert into curriculums_areas_interes values(4,123456789,1);
+insert into curriculums_areas_interes values(6,123456789,1);
+insert into curriculums_areas_interes values(10,123456789,1);
+insert into curriculums_areas_interes values(15,123456789,1);
+insert into curriculums_areas_interes values(20,123456789,1);
+
+	
+insert into datos_academicos values(null,123456789,1,'Escuela Gatitos de Kitty','1995-03-05','2003-12-20','Primaria de Gatitos');
+insert into datos_academicos values(null,123456789,1,'Liceo de Gatos','2004-03-05','2007-12-20','Secundaria Hermosa de Gatitos');
+insert into datos_academicos values(null,123456789,1,'Universidad de Gatos','2008-03-05',null,'Universidad de Gatos');
+
+insert into historial_laboral values(null,123456789,1,'Veterinaria Gatitos Felices','Jefe de Cajas de Arenas','2010-01-10','2012-12-01','Limpieza y Orden de Cajas de Gatos');
+insert into historial_laboral values(null,123456789,1,'Sicario de Perros','Ejecutor','2012-02-11',null,'Asesinatas con armas de fuego');
+
+insert into idiomas values (null,'Ingles'); 
+insert into idiomas values (null,'Portugues'); 
+insert into idiomas values (null,'Frances'); 
+insert into idiomas values (null,'Aleman');
+insert into idiomas values (null,'Chino Mandarin');
+
+insert into manejo_idiomas values(1,123456789,1,'Basico');
+insert into manejo_idiomas values(3,123456789,1,'Avanzado');
+insert into manejo_idiomas values(5,123456789,1,'Medio');
+
+insert into empresas values (987654321,'gatos.inc@gatin.com',AES_ENCRYPT('gatin','gatin'),'Gatos INC',69,'Gatunesca','Exportacion de Gatos Menores de Edad','Kitty #32, Catland','Empresa dedicada a la exportacion de gatitos menores de edad');
+
+insert into profesores values (111111111,'don.gato@gatouv.cat',AES_ENCRYPT('gatin','gatin'),'Gaston','Gatuso','Física','Magister En Ciencias Cuanticas de los Gatos');
+
+insert into recomendaciones values(111111111,123456789,'Un gato muy esforzado a la hora de jugar con lana');
+
+
+insert into asignaturas values (100,'Algebra Elemental',0,0,0);
+insert into asignaturas values (101,'Cálculo Diferencial',0,0,0);
+insert into asignaturas values (102,'Fundamentos de Programación',0,0,0);
+insert into asignaturas values (103,'Historia General de las Ciencias y las Tecnologías',0,0,0);
+insert into asignaturas values (104,'Formación Valórica Personal',0,0,0);
+insert into asignaturas values (110,'Física',0,0,0);
+insert into asignaturas values (111,'Cálculo Integral',0,0,0);
+insert into asignaturas values (112,'Programación I',1,0,0);
+insert into asignaturas values (113,'Introducción al Hardware',0,0,1);
+insert into asignaturas values (114,'Filosofía de las Ciencias',0,0,0);
+insert into asignaturas values (200,'Física Experimental',0,0,0);
+insert into asignaturas values (201,'Cálculo Multivariable',0,0,0);
+insert into asignaturas values (202,'Programación II',1,0,0);
+insert into asignaturas values (203,'Sistemas Digitales',0,0,1);
+insert into asignaturas values (204,'Estructuras Discretas',0,0,0);
+insert into asignaturas values (205,'Asignatura de Formación General I',0,0,0);
+insert into asignaturas values (210,'Álgebra Lineal',0,0,0);
+insert into asignaturas values (211,'Estructura de Datos',1,1,0);
+insert into asignaturas values (212,'Arquitectura de Computadores',0,0,1);
+insert into asignaturas values (213,'Teoría de Sistemas',0,0,0);
+insert into asignaturas values (214,'Contabilidad',0,0,0);
+insert into asignaturas values (215,'Asignatura de Formación General II',0,0,0);
+insert into asignaturas values (300,'Ecuaciones Diferenciales',0,0,0);
+insert into asignaturas values (301,'Electromagnetismo',0,0,0);
+insert into asignaturas values (302,'Análisis y Diseño de Algoritmos',1,0,0);
+insert into asignaturas values (303,'Teoría de Sistemas Operativos',0,0,1);
+insert into asignaturas values (304,'Fundamentos de Ingeniería de Software',1,0,0);
+insert into asignaturas values (305,'Inglés I',0,0,0);
+insert into asignaturas values (310,'Probabilidad y Estadística',0,0,0);
+insert into asignaturas values (311,'Lenguaje y Autómatas',1,1,0);
+insert into asignaturas values (312,'Redes de Computadores',0,0,1);
+insert into asignaturas values (313,'Metodoligías de Análisis',1,0,0);
+insert into asignaturas values (314,'Administración en Informática',0,0,0);
+insert into asignaturas values (315,'Inglés II',0,0,0);
+insert into asignaturas values (400,'Física Contemporánea',0,0,0);
+insert into asignaturas values (401,'Interfaz Hombre-Máquina',1,0,0);
+insert into asignaturas values (402,'Modelo de Datos',0,1,0);
+insert into asignaturas values (403,'Taller de Sistemas Operativos',0,0,1);
+insert into asignaturas values (404,'Metodologías de Diseño',1,0,0);
+insert into asignaturas values (405,'Inglés III',0,0,0);
+insert into asignaturas values (410,'Biología',0,0,0);
+insert into asignaturas values (411,'Desarrollo Web',1,0,0);
+insert into asignaturas values (412,'Sistema de Base de Datos',0,1,0);
+insert into asignaturas values (413,'Arquitectura de Software',1,0,0);
+insert into asignaturas values (414,'Evaluación de Proyectos',1,0,0);
+insert into asignaturas values (415,'Lenguajes de Programación',1,1,0);
+insert into asignaturas values (500,'Pruebas de Software',1,0,0);
+insert into asignaturas values (503,'Sistema de Telecomuniaciones',0,0,1);
+insert into asignaturas values (504,'Gestión de Proyectos Informáticos',1,0,0);
+insert into asignaturas values (505,'Investigación de Operaciones',0,0,0);
+insert into asignaturas values (510,'Economía',0,0,0);
+insert into asignaturas values (513,'Fundamentos de Inteligencia Artificial',0,0,0);
+insert into asignaturas values (514,'Taller de Aplicaciones',1,1,1);
+insert into asignaturas values (515,'Simulación',0,0,0);
+insert into asignaturas values (602,'Ética y Legislación',0,0,0);
+insert into asignaturas values (610,'Seminario de Tecnologías de Información y Comunicación',1,1,1);
+insert into asignaturas values (700,'Taller de Base de Datos',0,1,0);
+insert into asignaturas values (701,'Bases de Datos Avanzadas',0,1,0);
+insert into asignaturas values (702,'Minería de Datos',0,1,0);
+insert into asignaturas values (703,'Recuperación de Información',0,1,0);
+insert into asignaturas values (710,'Bibliotecas Digitales',1,0,0);
+insert into asignaturas values (711,'Productividad y Calidad de Software',1,0,0);
+insert into asignaturas values (712,'Computación Gráfica',1,0,0);
+insert into asignaturas values (720,'Taller de Redes',0,0,1);
+insert into asignaturas values (721,'TCP/IP',0,0,1);
+insert into asignaturas values (722,'Administración de Sistemas',0,0,1);
+insert into asignaturas values (723,'Calidad de Servicio en Redes',0,0,1);
+
+insert into notas_alumnos values(123456789,100,5.2);
+insert into notas_alumnos values(123456789,101,6.0);
+insert into notas_alumnos values(123456789,102,4.2);
+insert into notas_alumnos values(123456789,103,5.1);
+insert into notas_alumnos values(123456789,104,5.8);
+insert into notas_alumnos values(123456789,110,5.9);
+insert into notas_alumnos values(123456789,111,6.2);
+insert into notas_alumnos values(123456789,112,7.0);
+insert into notas_alumnos values(123456789,113,4.2);
+insert into notas_alumnos values(123456789,114,4.0);
+insert into notas_alumnos values(123456789,200,5.2);
+insert into notas_alumnos values(123456789,201,5.5);
+insert into notas_alumnos values(123456789,202,5.8);
+insert into notas_alumnos values(123456789,203,5.9);
