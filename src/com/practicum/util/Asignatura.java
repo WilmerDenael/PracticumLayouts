f90b1740e09e4840ec99470666e1c84de9a6f13d
LethalDrumsXxl <s.segovia.c@gmail.com>
2013-12-09 15:09:41 -0400

    v1.29
    Carga de asignaturas y objeto respectivo en util

---
+++ src/com/practicum/util/Asignatura.java
@@ -0,0 +1,42 @@
+package com.practicum.util;
+
+public class Asignatura {
+	private String asignatura;
+	private int nota;
+	private float desarrollo;
+	private float baseDeDatos;
+	private float redes;
+	
+	public String getAsignatura() {
+		return asignatura;
+	}
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+	public float getDesarrollo() {
+		return desarrollo;
+	}
+	public void setDesarrollo(float desarrollo) {
+		this.desarrollo = desarrollo;
+	}
+	public float getBaseDeDatos() {
+		return baseDeDatos;
+	}
+	public void setBaseDeDatos(float baseDeDatos) {
+		this.baseDeDatos = baseDeDatos;
+	}
+	public float getRedes() {
+		return redes;
+	}
+	public void setRedes(float redes) {
+		this.redes = redes;
+	}
+	
+	
+}
